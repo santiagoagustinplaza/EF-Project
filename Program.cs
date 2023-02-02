3492b6a652859fd20f9eb21f10445ea5fb6882e7
Santiago Agustin Plaza <santiagoagustinplaza@gmail.com>
2023-02-02 15:41:25 -0300

    database in memory testing

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
+using efproject;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TasksContext> (p => p.UseInMemoryDatabase("DbTasks"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconection", async([FromServices] TasksContext dbContext) =>
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Database in Memory: "+ dbContext.Database.IsInMemory());
+});
 
 app.Run();
