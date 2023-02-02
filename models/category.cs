03d11548223ceb17602ff01fb4ef06fbabf2368e
Santiago Agustin Plaza <santiagoagustinplaza@gmail.com>
2023-02-02 12:52:04 -0300

    models mapping

--- models/category.cs
+++ models/category.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace efproject.Models;
 public class Category
 {
+    [Key]
     public Guid CategoryID { get; set; }
+    [Required]
+    [MaxLength(150)]
     public string Name { get; set; }
     public string Description { get; set; }
     public virtual ICollection<Task> Tasks{ get; set; }
--- models/task.cs
+++ models/task.cs
@@ -1,13 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace efproject.Models;
 public class Task
 {
+    [Key]
     public Guid TaskId { get; set; }
+    [ForeignKey("CategoryId")]
     public Guid CategoryId { get; set; }
+    [MaxLength(200)]
     public string Title { get; set; }
     public string Description { get; set; }
     public Priority TaskPriority { get; set; }
     public DateTime CreationDate { get; set; }
     public virtual Category Category { get; set; }
+    [NotMapped]
+    public string Resume { get; set; }
 }
 public enum Priority
 {
